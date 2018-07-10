73e987c2ebecdde2b4f749dc59ba352dd567b894
owaki <owaki@framelunch.jp>
2018-07-10 16:15:24 +0900

    文字コード

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <!-- This file allows you to host this page as a static file on Heroku -->
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php 
+    ini_set('mbstring.internal_encoding' , 'UTF-8');
+    header( 'Location: /index.html' ) ;  
+?>
