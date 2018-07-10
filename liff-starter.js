a3a3eab3a4610784003fa30b33ec7e0425f1f7d0
owaki <owaki@framelunch.jp>
2018-07-10 15:19:01 +0900

    change

--- liff-starter.js
+++ liff-starter.js
@@ -71,3 +71,38 @@ function toggleProfileData() {
         elem.style.display = "block";
     }
 }
+window.onload = function (e) {
+    liff.init(function (data) {
+        initializeApp(data);
+    });
+};
+function initializeApp(data) {
+    var color = '#' + parseInt(Math.random() * 0xffffff).toString(16);
+    document.getElementById('sendmessagebutton').addEventListener('click', function () {
+        liff.sendMessages([
+            {
+                type: "template",
+                altText: "ラッキーカラー",
+                template: {
+                    type: 'buttons',
+                    thumbnailImageUrl: 'https://{herokuに追加したウェブアプリのURL}/img/spacer.gif',//背景はファイルサイズ抑えるため透過のGIF画像を入れて見ました
+                    imageAspectRatio: 'square',
+                    imageSize: 'contain',
+                    imageBackgroundColor: color,
+                    text: color + ' がラッキーそうです。',
+                    actions: [//トーク内に表示される投稿からのリンクが設定できる
+                        {
+                            type: "uri",
+                            label: "ラッキーカラーを見てみよう",
+                            uri: "line://app/{liffId}"//ここに先ほど取得したliffIdを
+                        }
+                    ]
+                }
+            }
+        ]).then(function () {
+            liff.closeWindow();
+        }).catch(function (error) {
+            window.alert("Error sending message: " + error);
+        });
+    });
+}
