d92211c2843ef29d9390104cd2210227837481df
owaki <owaki@framelunch.jp>
2018-07-10 16:08:10 +0900

    色診断機能をつけてみた

--- liff-starter.js
+++ liff-starter.js
@@ -1,108 +1,35 @@
 window.onload = function (e) {
-    liff.init(function (data) {
-        initializeApp(data);
-    });
-};
-
-function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
-
-    // openWindow call
-    document.getElementById('openwindowbutton').addEventListener('click', function () {
-        liff.openWindow({
-            url: 'https://line.me'
-        });
-    });
-
-    // closeWindow call
-    document.getElementById('closewindowbutton').addEventListener('click', function () {
-        liff.closeWindow();
-    });
-
-    // sendMessages call
-    document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'text',
-            text: "You've successfully sent a message! Hooray!"
-        }, {
-            type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
-        }]).then(function () {
-            window.alert("Message sent");
-        }).catch(function (error) {
-            window.alert("Error sending message: " + error);
-        });
-    });
-
-    //get profile call
-    document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.getProfile().then(function (profile) {
-            document.getElementById('useridprofilefield').textContent = profile.userId;
-            document.getElementById('displaynamefield').textContent = profile.displayName;
-
-            var profilePictureDiv = document.getElementById('profilepicturediv');
-            if (profilePictureDiv.firstElementChild) {
-                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-            }
-            var img = document.createElement('img');
-            img.src = profile.pictureUrl;
-            img.alt = "Profile Picture";
-            profilePictureDiv.appendChild(img);
-
-            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
-    });
+  liff.init(function (data) {
+    initializeApp(data)
+  })
 }
-
-function toggleProfileData() {
-    var elem = document.getElementById('profileinfo');
-    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-        elem.style.display = "none";
-    } else {
-        elem.style.display = "block";
-    }
-}
-window.onload = function (e) {
-    liff.init(function (data) {
-        initializeApp(data);
-    });
-};
-function initializeApp(data) {
-    var color = '#' + parseInt(Math.random() * 0xffffff).toString(16);
-    document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        liff.sendMessages([
+function initializeApp (data) {
+  var color = '#' + parseInt(Math.random() * 0xffffff).toString(16)
+  document.getElementById('sendmessagebutton').addEventListener('click', function () {
+    liff.sendMessages([
+      {
+        type: 'template',
+        altText: 'ラッキーカラー',
+        template: {
+          type: 'buttons',
+          thumbnailImageUrl: 'https://{herokuに追加したウェブアプリのURL}/img/spacer.gif', // 背景はファイルサイズ抑えるため透過のGIF画像を入れて見ました
+          imageAspectRatio: 'square',
+          imageSize: 'contain',
+          imageBackgroundColor: color,
+          text: color + ' がラッキーそうです。',
+          actions: [ // トーク内に表示される投稿からのリンクが設定できる
             {
-                type: "template",
-                altText: "ラッキーカラー",
-                template: {
-                    type: 'buttons',
-                    thumbnailImageUrl: 'https://{herokuに追加したウェブアプリのURL}/img/spacer.gif',//背景はファイルサイズ抑えるため透過のGIF画像を入れて見ました
-                    imageAspectRatio: 'square',
-                    imageSize: 'contain',
-                    imageBackgroundColor: color,
-                    text: color + ' がラッキーそうです。',
-                    actions: [//トーク内に表示される投稿からのリンクが設定できる
-                        {
-                            type: "uri",
-                            label: "ラッキーカラーを見てみよう",
-                            uri: "line://app/{liffId}"//ここに先ほど取得したliffIdを
-                        }
-                    ]
-                }
+              type: 'uri',
+              label: 'ラッキーカラーを見てみよう',
+              uri: 'line://app/1592931160-eddxlPXq' // ここにliffId
             }
-        ]).then(function () {
-            liff.closeWindow();
-        }).catch(function (error) {
-            window.alert("Error sending message: " + error);
-        });
-    });
+          ]
+        }
+      }
+    ]).then(function () {
+      liff.closeWindow()
+    }).catch(function (error) {
+      window.alert('Error sending message: ' + error)
+    })
+  })
 }
